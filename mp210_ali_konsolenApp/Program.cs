f689efb7e99efa82225feeca7f33e7418502e74c
Taha Ali <taha.ali@i-azo.ch>
2022-07-13 12:53:10 +0200

    Projektdateien hinzufügen.

---
+++ mp210_ali_konsolenApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mp210_ali_konsolenApp
+{
+    public class MainApplication
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Geben Sie ihren Namen ein: ");
+                string sName = Console.ReadLine();
+                Console.WriteLine("Willkommen " + sName + "!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("FEHLER: " + ex.Message);
+            }
+        }
+    }
+}
+
